e2d271f2f10041f4ac1a898f84a78336301debad
Boateng Prince Agyenim <isbbydior@gmail.com>
2026-09-16 00:57:00 +0000

    chore: removed explore fellowships button from about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -63,12 +63,6 @@ export default function AboutPage() {
           />
           <figcaption>Real conversations, not just check-in sheets.</figcaption>
         </figure>
-
-        <div style={{ marginTop: "40px", paddingTop: "24px", borderTop: "1px solid var(--border)" }}>
-          <Link className="button button-primary" href="/student">
-            Explore fellowships
-          </Link>
-        </div>
       </div>
       <SiteFooter />
     </PageShell>
